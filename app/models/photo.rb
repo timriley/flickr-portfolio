3b97391cd906940818e6add45850f223315e9080
Tim Riley <tim@kagura.local>
2008-05-08 00:21:12 +1000

    refactor the Photo class into Photo and FlickrPhoto

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,73 +10,44 @@ class Photo < ActiveRecord::Base
   def hidden?
     !self.active?
   end
-  
-  def update_attributes_from_flickr
-    self.attributes.merge!(Photo.flickr_photo_attributes(self.flickr_id))
+    
+  def self.new_from_flickr_photo(flickr_photo)
+    Photo.new(flickr_photo.attributes)
   end
   
-  def self.flickr_photos_by_user_and_tag(nsid, tag)
-    flickr.photos.search(:user_id => nsid, :tags => tag, :extras => 'last_update').collect { |photo| [photo.id, photo.lastupdate] }
-  end
-    
-  def self.flickr_photo_attributes(flickr_id)
-    attrs = {}
-    
-    info = flickr.photos.getInfo(:photo_id => flickr_id)
-    urls = flickr.photos.getSizes(:photo_id => flickr_id)
-    
-    attrs[:flickr_photo_id]   = info.id
-    attrs[:title]             = info.title
-    attrs[:description]       = info.description
-    attrs[:taken_at]          = Time.parse(info.dates.taken) if info.dates.taken # not everything has exif data
-    attrs[:flickr_updated_at] = info.dates.lastupdate
-    
-    urls.each do |u|
-      case u.label
-      when 'Thumbnail':
-        attrs[:thumb_source_url] = u.source.gsub('\\','').gsub('http: ', 'http:')
-      when 'Medium':
-        attrs[:medium_source_url] = u.source.gsub('\\','').gsub('http: ', 'http:')
-      when 'Original':
-        attrs[:fullsize_source_url] = u.source.gsub('\\','').gsub('http: ', 'http:')
-      end
-    end
-    
-    attrs
+  def self.create_from_flickr_photo(flickr_photo)   
+    Photo.create(flickr_photo.attributes)
   end
   
-  def self.new_from_flickr(flickr_id)
-    photo = Photo.new
-    photo.attributes = Photo.flickr_photo_attributes(flickr_id)
-    photo
+  def update_from_flickr!
+    self.attributes.merge!(FlickrPhoto.new(self.flickr_id).attributes)
   end
   
-  def self.create_from_flickr(flickr_id)   
-    photo = Photo.new_from_flickr(flickr_id)
-    photo.save
-  end
+  # def self.flickr_photos_by_user_and_tag(user_nsid, tag)
+    
+  # def self.flickr_photo_attributes(flickr_id)
   
+  # FIXME THIS NEEDS UPDATING TO WORK WITH FlickrPhoto
   def self.flickr_sync_by_user_and_tag(nsid, tag)
     flickr_photos = Photo.flickr_photos_by_user_and_tag(nsid, tag)
-    
-    photos = Photo.find(:all)
+    local_photos  = Photo.find(:all)
     
     # sort all of the current photos by flickr id
-    photos_by_flickr_id = {}
-    photos.each do |photo|
+    local_photos_by_flickr_id = {}
+    local_photos.each do |photo|
       photos_by_flickr_id[photo.flickr_id] = photo
     end
     
-    flickr_photos.each do |flickr_photo|
-      if !photos_by_flickr_id.keys.include?(flickr_photo[0])
-        # If the photo does not exist among the ones from the db, create it.
-        Photo.create_from_flickr_id(flickr_photo[0])
-      elsif photos_by_flickr_id[flickr_photo[0]].flickr_updated_at.to_i < flickr_photo[1].to_i
-        # If we've imported the photo before, update it if it has been updated on flickr
-        photo = photos_by_flickr_id[flickr_photo[0]]
-        photo.update_attributes_from_flickr(flickr_photo[0])
-        photo.save
+    flickr_photos.each do |photo_info|
+      if !local_photos_by_flickr_id[photo_info[0]]
+        # if the photo does not exist locally, create it.
+        Photo.create_from_flickr_id(photo_info[0])
+      elsif local_photos_by_flickr_id[photo_info[0]].flickr_updated_at.to_i < photo_info[1].to_i
+        # if we've imported the photo before, but it has been since updated on flickr, update it locally.
+        local_photo = local_photos_by_flickr_id[photo_info[0]]
+        local_photo.update_attributes_from_flickr(photo_info[0])
+        local_photo.save
       end
     end
-  end
+  end  
 end
