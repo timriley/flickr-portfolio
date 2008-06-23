a86c05a646a322281ce40aed2eb869e3071b21c7
Tim Riley <tim@openmonkey.com>
2008-06-23 23:38:54 +1000

    added some more support for a single login

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,5 @@
+class Admin::AdminController < ApplicationController
+  include SingleLogin
+  
+  before_filter :login_required
+end
--- app/controllers/admin/logins_controller.rb
+++ app/controllers/admin/logins_controller.rb
@@ -1,4 +1,21 @@
-class LoginsController < ApplicationController
+class Admin::LoginsController < ApplicationController
+  def new
+  end
   
+  def create
+    if Settings.instance.crypted_password?(params[:password])
+      session[:logged_in] == true
+      redirect_back_or_default('/')
+      flash[:notice] = "Logged in successfully"
+    else
+      flash[:error] = 'Incorrect password'
+      render :action => 'new'
+    end
+  end
   
+  def destroy
+    reset_session
+    flash[:notice] = 'Logged out'
+    redirect_back_or_default('/')
+  end
 end
