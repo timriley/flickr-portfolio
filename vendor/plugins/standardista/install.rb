e5f92f63339a94f2259db7dda8026468d3196c2f
Tim Riley <tim@openmonkey.com>
2008-12-15 22:09:27 +1100

    remove standardista, rspec, rspec_on_rails plugins

--- vendor/plugins/standardista/install.rb
+++
@@ -1,5 +0,0 @@
-begin
-  gem 'haml', '~> 2.0'
-rescue Gem::LoadError
-  $stderr.puts 'Standardista plugin needs Haml 2.0.'
-end
--- vendor/plugins/standardista/tasks/standardista_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :standardista do
-#   # Task goes here
-# end
